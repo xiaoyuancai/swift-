60505bf4660c9c8efb94b8ffa31712048f8f018c
xiaoyuancai <hudong@hudongdeMacBook-Pro.local>
2016-04-04 17:42:08 +0800

    添加工程文件，swift学习在playground中

---
+++ gello world/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  gello world
+//
+//  Created by 互动 on 16/4/4.
+//  Copyright © 2016年 互动. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.blueColor()
+        //let 类型只是地址不可变，属性是可以改变的,let变量编译器会做出进一步的优化
+        let label:UILabel = UILabel.init(frame: CGRectMake(0, 0, 320, 50))
+        label.backgroundColor = UIColor.yellowColor()
+        self.view.addSubview(label)
+        label.frame = CGRectMake(20, 64, 100, 100)
+        label.text = "hello xiaoyuancai"
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
